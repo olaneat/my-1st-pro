e63438e80a8b08beb79ea42919a9aa37e81c3155
OlaNeat Ayoola <olaneat@yahoo.com>
2017-05-31 22:16:10 +0100

    Add files via upload
    help me check this i fill this where d error is

--- urls.py
+++ urls.py
@@ -1,8 +1,4 @@
+from django.conf.urls import url
+from . import views
 
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    (r'^admin/', admin.site.urls),
-    (r'^music/', include('music.urls'))
-]
+urlpathern = (r'^music/$', views.index),
