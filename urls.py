e4e7bd7ea2616f7156d481452ff746b7153f1151
Timothy Olaleke <timothyolaleke@gmail.com>
2017-06-01 05:32:06 +0100

    Updates URL.py as list and corrected typo

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-urlpathern = (r'^music/$', views.index),
+#urlpattern should be handled as a list containing tuples
+urlpattern = [
+    url(r'^music/$', views.index),
+    ]
